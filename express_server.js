c0bf9c57bfae7e167c8c6415f3429524652fe105
Alastair <alastair05@gmail.com>
2022-03-22 08:55:52 -0700

    Added random new url generator

--- express_server.js
+++ express_server.js
@@ -9,6 +9,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateRandomString = function() {
+  return Math.random().toString(32).substring(2, 5);
+};
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -28,6 +37,12 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.render(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
